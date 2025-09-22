8d4b4935928fe81c34331a28aa30ce1ee63c7b63
Marshy <affan.telek@gmail.com>
2025-09-22 00:19:41 -0400

    Lab 3 part 1

--- COMP2139-ICE/Controllers/ProjectController.cs
+++ COMP2139-ICE/Controllers/ProjectController.cs
@@ -1,17 +1,29 @@
+using COMP2139_ICE.Data;
 using COMP2139_ICE.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace COMP2139_ICE.Controllers;
 
 public class ProjectController: Controller
 {
+    
+    private readonly ApplicationDbContext _context;
+
+    public ProjectController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
     [HttpGet]
         
         public IActionResult Index()
     {
+        /*
         var projects = new List<Project>
         {
             new Project { ProjectId = 1, Name = "Project 1", Description = "First Project" },
         };
+        */
+        var projects = _context.Projects.ToList();
         return View(projects);
     }
     
@@ -29,8 +41,14 @@ public class ProjectController: Controller
         
         [HttpGet]
         public IActionResult Details(int id)
-        {
+        { /*
             var project = new Project{ ProjectId = id, Name ="Project " + id, Description ="Project " + id};
+            */
+            var project = _context.Projects.FirstOrDefault(p=> p.ProjectId == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
             return View(project);
         }
 }        
